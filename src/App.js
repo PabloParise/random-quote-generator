1439f9e6724d9198ab065aebf3783648666c52aa
Pablo Parise <pabloeparise@gmail.com>
2022-04-26 18:02:07 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import './App.css';
+import { faDigitalTachograph } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const [quotes, setQuotes] = useState('');     //Hook for setting the state and updating it
+
+  const colorRef = useRef();
+  let colors = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+  ];
+
+  const APIURL = 'https://type.fit/api/quotes'; //The API url
+
+  const getQuote = () => { 
+    fetch(APIURL)
+      .then(response => response.json()) 
+      .then(data => {
+        let randomNum = Math.floor(Math.random() * data.length);
+        setQuotes(data[randomNum]);
+      })
+      .catch(err => console.error(err));
+  };                                            //Function for  
+
+  useEffect(() => {
+    getQuote();
+  }, []); //This is for getting a random quote when refreshing the page
+
+  useEffect(() => {
+    colorRef.current.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+  }, [quotes]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content" ref={colorRef}>
+        <Card id='quote-box'>
+          <Card.Body>
+            <blockquote id='text'>
+              <p>
+                {quotes.text}
+              </p>
+              <footer className="blockquote-footer" id='author'>
+                <cite>{quotes.author}</cite>
+              </footer>
+            </blockquote>
+          <Card.Footer id="card-footer">
+            <Button id='new-quote' variant='outline-dark' onClick={getQuote}>
+              New Quote
+            </Button>
+            <a id='tweet-quote' href={'https://twitter.com/intent/tweet'} target="_blank" title='Tweet Quote'>
+            <FontAwesomeIcon icon={faTwitter} border id='twitter-icon'/>
+            </a>
+          </Card.Footer>
+          </Card.Body>
+        </Card>
+      </section>
     </div>
   );
 }
